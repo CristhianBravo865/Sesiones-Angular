8ad760a9b0ac169dc5ac620dca75657c273c58d8
Cristhian <cristhian.bravo.8657@gmail.com>
2025-07-04 16:11:10 -0500

    Angular2: Se implementa una lista de videos con links de youtube embebidos

--- angular_bootstrap/src/app/app.ts
+++ angular_bootstrap/src/app/app.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  videoList: { name: string; slug: string; embed: string; url: string; safeUrl?: SafeResourceUrl }[] = [];
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.videoList = [
+      {
+        name: 'Video 1',
+        slug: 'video-1',
+        embed: '',
+        url: 'https://www.youtube.com/embed/pBwedBeawZs'
+      },
+      {
+        name: 'Video 2',
+        slug: 'video-2',
+        embed: '',
+        url: 'https://www.youtube.com/embed/9PY_qyQ1S7M'
+      }
+    ];
+
+    // Sanear las URLs
+    this.videoList.forEach(item => {
+      item.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(item.url);
+    });
+  }
+
+  getEmbedUrl(item: any): SafeResourceUrl {
+    return item.safeUrl!;
+  }
+}
